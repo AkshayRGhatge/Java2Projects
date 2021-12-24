843662198be76461a8f659f0e24609c31b310c5d
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-24 12:21:38 -0500

    Adding InterfaceEx

---
+++ Week8AnimalPlanet/src/Dog.java
@@ -0,0 +1,27 @@
+
+/**
+ */
+public class Dog extends Mammal implements INameAble  {
+
+	
+
+	public Dog(int age)
+	{
+		super(age);
+
+	}
+	
+	@Override
+	public void setName()
+	{
+	
+	}
+	@Override
+	public String getName()
+	{
+		return null;
+	}
+
+
+
+}
---
+++ Week8AnimalPlanet/src/Mammal.java
@@ -0,0 +1,23 @@
+
+/**
+ */
+public class Mammal {
+
+	protected int age;
+
+	public Mammal(int age)
+	{
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+}
